65a4662982fc63f39d5266edf50f2c98606e9ec9
John Stangel <jstangel@bellarmine.edu>
2024-06-18 16:02:39 -0500

    Update Java script in Application with infrastructure code.

---
+++ Application.java
@@ -0,0 +1,17 @@
+public class Application {
+
+public static void main(String args[]){
+
+System.out.println("My Application");
+
+System.out.println("==============");
+
+
+System.out.println("\n\n\n\n\n\n\n\n\n\n");
+
+System.out.println("=======================");
+
+
+}
+
+}
